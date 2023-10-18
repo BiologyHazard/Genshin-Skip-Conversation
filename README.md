2a6396b4c204b3988ca71ba7a45d29f0dbe33f6c
BioHazard <3482991796@qq.com>
2023-10-18 17:52:24 +0800

    modified:   README.md 	deleted:    enhance_artifacts.py 	new file:   genshin_assistant.py 	deleted:    skip_conversation.py

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Genshin-Skip-Conversation
 原神、崩坏：星穹铁道自动跳过对话
 
-使用方法：运行skip_conversation.py
+原神自动强化圣遗物
 
-游戏在前台时，按p键开启，再次按p键关闭
+# 使用方法
+
+运行 genshin_assistant.py
+
+## 跳过对话
+
+游戏在前台时，按 `p` 键开启，再次按 `p` 键关闭
+
+## 原神强化圣遗物
+
+在强化页面，按 `[` 键
---
+++ genshin_assistant.py
@@ -0,0 +1,68 @@
+import time
+import winsound
+from pathlib import Path
+from typing import Never
+
+import pyautogui
+import pygetwindow
+import pynput
+
+enable_sound_path: Path = Path(__file__).parent / 'sound/enable.wav'
+disable_sound_path: Path = Path(__file__).parent / 'sound/disable.wav'
+
+skip_conversation_clicks: tuple[float, float] = (0.7160, 0.7444)
+skip_conversation_supported_titles: list[str] = ['原神', '崩坏：星穹铁道', 'Genshin Impact']
+skip_conversation_key_code = pynput.keyboard.KeyCode(char='p')
+skip_conversation_click_interval: float = 0.1  # 单位秒
+
+
+enhance_aritifact_clicks: list[tuple[float, float]] = [
+    (241/3268, 433/1892),
+    (2973/3268, 1367/1892),
+    (2953/3268, 1787/1892),
+    (244/3268, 302/1892),
+]
+enhance_aritifact_supported_titles: list[str] = ['原神', 'Genshin Impact']
+enhance_aritifact_key_code = pynput.keyboard.KeyCode(char='[')
+
+
+def main() -> Never:
+    def on_press(key) -> None:
+        nonlocal on
+        if key == skip_conversation_key_code:
+            window = pygetwindow.getActiveWindow()
+            if window is not None and window.title in skip_conversation_supported_titles:
+                on = not on
+                if on:
+                    winsound.PlaySound(str(enable_sound_path), flags=1)
+                    print('Enabled')
+                else:
+                    winsound.PlaySound(str(disable_sound_path), flags=1)
+                    print('Disabled')
+        if key == enhance_aritifact_key_code:
+            window = pygetwindow.getActiveWindow()
+            if window is not None and window.title in enhance_aritifact_supported_titles:
+                for mx, my in enhance_aritifact_clicks:
+                    x: int = round(window.left + mx * window.width)
+                    y: int = round(window.top + my * window.height)
+                    # pyautogui.moveTo(x, y)
+                    pyautogui.leftClick(x, y)
+                    # time.sleep(0.1)
+
+    on: bool = False
+    listener = pynput.keyboard.Listener(on_press=on_press)
+    listener.start()
+    while True:
+        if on:
+            window = pygetwindow.getActiveWindow()
+            if window is not None and window.title in skip_conversation_supported_titles:
+                mx, my = skip_conversation_clicks
+                x: int = round(window.left + mx * window.width)
+                y: int = round(window.top + my * window.height)
+                # pyautogui.moveTo(x, y)
+                pyautogui.leftClick(x, y)
+        time.sleep(skip_conversation_click_interval)
+
+
+if __name__ == '__main__':
+    main()
